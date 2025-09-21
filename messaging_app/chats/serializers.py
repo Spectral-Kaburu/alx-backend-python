b19bbc0439b4ed68b8c268d85f847f40ffab4f5a
Lee Kaburu <spectre@BLACKBOXX.SPECTRAL>
2025-09-21 22:30:51 +0300

    feat: Serializers for the models

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from .models import CustomUser, Message, Conversation
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'created_at']
+        read_only_fields = ['user_id', 'created_at']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email', 'password', 'phone_number', 'role']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        read_only_fields = ['message_id', 'sent_at', 'sender']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'messages', 'created_at']
+        read_only_fields = ['conversation_id', 'created_at']
+
+class ConversationCreateSerializer(serializers.ModelSerializer):
+    participant_ids = serializers.ListField(
+        child=serializers.UUIDField(),
+        write_only=True
+    )
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participant_ids', 'created_at']
+        read_only_fields = ['conversation_id', 'created_at']
+
+    def create(self, validated_data):
+        participant_ids = validated_data.pop('participant_ids')
+        conversation = Conversation.objects.create(**validated_data)
+        users = CustomUser.objects.filter(user_id__in=participant_ids)
+        conversation.participants.set(users)
+        return conversation

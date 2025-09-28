b3a5f23460f6247b69da2fe922e1ae043e8282d5
Lee Kaburu <spectre@BLACKBOXX.SPECTRAL>
2025-09-28 23:15:46 +0300

    Set up auth urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,20 +1,24 @@
 from django.urls import path, include
-from rest_framework_nested import routers
+from rest_framework_nested import routers as rts
 from .views import ConversationViewSet, MessageViewSet
+from .auth import RegisterView, LoginView, RefreshView
 
+con = 'conversation'
 # DRF routers
-router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
+router = rts.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename=con)
 
 # Nested router for messages under conversations
-conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
-conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+conrts = rts.NestedDefaultRouter(router, r'conversations', lookup=con)
+conrts.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView, name="login"),
+    path("refresh/", RefreshView, name="token_refresh"),
     path('', include(router.urls)),
-    path('', include(conversations_router.urls)),
+    path('', include(conrts.urls)),
 ]
-
 
 
 """from rest_framework_nested import routers
@@ -22,12 +26,12 @@ from .views import ConversationViewSet, MessageViewSet
 
 # Root router for conversations
 router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'conversations', ConversationViewSet, basename=con)
 
 # Nested router for messages under conversations
-conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
-conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+conrts = routers.NestedDefaultRouter(router, r'conversations', lookup=con)
+conrts.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 # Export both sets of URLs
-urlpatterns = router.urls + conversations_router.urls
+urlpatterns = router.urls + conrts.urls
 """

ddde98810a1a8ac9e3df6648fc38db917069f9cb
Lee Kaburu <spectre@BLACKBOXX.SPECTRAL>
2025-09-28 23:20:13 +0300

    Allow permissions for owners only

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+
+class IsOwner(BasePermission):
+    """
+    Allow access only to the owner of the object.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user

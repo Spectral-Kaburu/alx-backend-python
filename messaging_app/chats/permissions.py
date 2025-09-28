530f21de858c03a2a982cc15b8f4f78de809a65c
Lee Kaburu <spectre@BLACKBOXX.SPECTRAL>
2025-09-28 23:46:37 +0300

    update the whole thing

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,9 +1,25 @@
 from rest_framework import permissions
+from .models import Conversation
 
-class IsOwner(permissions.BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     """
-    Allow access only to the owner of the object.
+- Only authenticated users can access the API.
+- Only participants of a conversation can send/view/update/delete messages.
     """
+
+    def has_permission(self, request, view):
+        # Must be logged in
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+        """
+        Check if the user is a participant in the conversation.
+        Works for both Conversation and Message objects.
+        """
+        if hasattr(obj, "participants"):  # conversation instance
+            return request.user in obj.participants.all()
+
+        if hasattr(obj, "conversation"):  # message instance
+            return request.user in obj.conversation.participants.all()
+
+        return False
